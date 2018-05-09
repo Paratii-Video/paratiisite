a726db8a853651c4886ce93214d9b0fda89722bb
jrgarou <jrgarou@gmail.com>
2018-05-08 21:50:52 -0300

    load http .json file

--- src/website/scripts/main.js
+++ src/website/scripts/main.js
@@ -1,14 +1,39 @@
+import axios from 'axios'
 import Vue from 'vue'
+import Vuex from 'vuex'
+import getters from './store/getters'
+import mutations from './store/mutations'
+import actions from './store/actions'
 import { router } from './routes'
-import store from './store'
 import App from './App.vue'
 
-new Vue({
-  el: '#app',
-  store,
-  router,
-  render: h => h(App),
-  beforeCreate () {
-    this.$store.dispatch('setNavigatorLang')
-  }
-})
+axios.get('/data/website.json')
+  .then(function (response) {
+    console.log(response)
+    let ParatiiData = response.data
+
+    Vue.use(Vuex)
+
+    new Vue({
+      el: '#app',
+      store: new Vuex.Store({
+        state: {
+          content: ParatiiData,
+          lang: 0,
+          showCover: true,
+          isNavOpen: false,
+          isOutOfTop: false,
+          isNavWhite: false,
+          isCampaignTheme: false
+        },
+        getters,
+        mutations,
+        actions
+      }),
+      router,
+      render: h => h(App)
+    })
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
