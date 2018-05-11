457affe4038e35aa01a3354003a1ec8128713612
jrgarou <jrgarou@gmail.com>
2018-05-11 00:38:14 -0300

    load website.json / add axios

--- src/website/scripts/main.js
+++ src/website/scripts/main.js
@@ -1,12 +1,38 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
-import store from './store'
+import getters from './store/getters'
+import mutations from './store/mutations'  
+import actions from './store/actions'
 import { router } from './routes'
 import App from './App.vue'
 
-new Vue({
-  el: '#app',
-  store,
-  router,
-  render: h => h(App)
-})
+axios.get('/data/website.json')
+  .then(function (response) {
+    let ParatiiData = response.data
+
+    Vue.use(Vuex)
+
+    new Vue({
+      el: '#app',
+      store: new Vuex.Store({
+        state: {
+          content: ParatiiData,
+          lang: 0,
+          showCover: true,
+          isNavOpen: false,
+          isOutOfTop: false,
+          isNavWhite: false,
+          isCampaignTheme: false
+        },
+        getters,
+        mutations,
+        actions
+      }),
+      router,
+      render: h => h(App)
+    })
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
