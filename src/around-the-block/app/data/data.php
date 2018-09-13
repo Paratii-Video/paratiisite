b22614036dca4ff598cd49218c73cb11a0610856
jrgarou <jrgarou@gmail.com>
2018-09-13 11:24:36 -0300

    Around the Block - Chapter 1 link

--- src/around-the-block/app/data/data.php
+++ src/around-the-block/app/data/data.php
@@ -85,7 +85,7 @@
         'sections' => [
             'banner' => [
                 'label' => '🕒 Ep. 1 launch',
-                'date' => '2018-09-18T15:00:00'
+                'date' => '2018-09-18T18:00:00'
             ],
             'event' => [
                 'title' => 'Event'
@@ -158,7 +158,7 @@
                 'alert' => 'Not available yet',
                 'items' => [
                     [
-                        'href' => 'https://lab.paratii.video/',
+                        'href' => 'https://lab.paratii.video/play/GLdcTbPq9fXb',
                         'label' => 'chapter_1',
                         'title' => 'The<br> Genesis<br> Block',
                         'text' => 'networks start from a single individual. individuals start from a single purpose.',
