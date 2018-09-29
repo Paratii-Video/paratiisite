e252945b4ae79b7553927f233d9a95025ac6d81e
jrgarou <jrgarou@gmail.com>
2018-09-28 21:24:54 -0300

    Around the Block - ep2 and new date to ep 3

--- src/around-the-block/app/scripts/app.js
+++ src/around-the-block/app/scripts/app.js
@@ -1246,11 +1246,11 @@ VideosPlayer.prototype.set = function(video) {
     function init () {
         GLOBALS.body.scrollTop = 0;
         
-        // if ($('.intro').length) {
-        //     intro(start);
-        // } else {
+        if ($('.intro').length) {
+            intro(start);
+        } else {
             start();
-        // }
+        }
     }
 
     init();
